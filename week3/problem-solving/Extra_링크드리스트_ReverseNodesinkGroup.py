aa8c24caca29e19383cc247669088c90848cdb72
JCBBBBBB <wjdcksqls1@naver.com>
2026-03-15 00:15:09 +0900

    03/14

--- week3/problem-solving/Extra_링크드리스트_ReverseNodesinkGroup.py
+++ week3/problem-solving/Extra_링크드리스트_ReverseNodesinkGroup.py
@@ -1,2 +1,38 @@
 # 링크드리스트 - Reverse Nodes in k-Group
 # 문제 링크: https://leetcode.com/problems/reverse-nodes-in-k-group/description/?envType=study-plan-v2&envId=top-interview-150
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+
+arr = [38, 27, 43, 3, 9, 82, 10]
+sorted_arr = merge_sort(arr)
+
+print("정렬 전:", arr)
+print("정렬 후:", sorted_arr)
--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,52 @@
 # 이분탐색 - 두 용액 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2470
+
+# ai 힌트 사용, 코드 x
+
+# 투포인터 사용 
+
+# 전체 용액의 수
+N = int(input())
+
+
+# -2 4 - 99 -1 98
+data = list(map(int, input().split()))
+
+
+data.sort() # -99 -2 -1 4 98
+
+
+l = 0 # 왼쪽 인덱스
+r = len(data) - 1 # 오른쪽 인덱스
+sum = data[l] + data[r] #맨 왼쪽 + 맨 오른쪽 합
+leftNum = data[l]
+rightNum = data[r]
+
+
+while(l < r):
+    if(data[l] + data[r] < 0):
+        l += 1
+        if(l >= r):
+            break
+        if(abs(sum) > abs(data[l] + data[r])):
+            leftNum = data[l]
+            rightNum = data[r]
+            sum = data[l] + data[r]
+
+    elif(data[l] + data[r] > 0):
+        r -= 1
+        if(l >= r):
+            break
+        if(abs(sum) > abs(data[l] + data[r])):
+            leftNum = data[l]
+            rightNum = data[r]
+            sum = data[l] + data[r]
+    else:
+        leftNum = data[l]
+        rightNum = data[r]
+        break
+
+minNum = min(leftNum, rightNum)
+maxNum = max(leftNum, rightNum)
+
+print(minNum, maxNum)
