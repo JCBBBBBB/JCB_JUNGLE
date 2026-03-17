6b3b823fd16a7feee0f465d60b0df1bb0b373b34
JCBBBBBB <wjdcksqls1@naver.com>
2026-03-17 17:24:33 +0900

    03/17

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,45 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+
+#x+y 가능한 모든값 저장
+#d 큰거부터 본다
+#d마다 z하나씩 넣어본다
+#d-z == x+y 이면 처음 d 출력
+
+#시간복잡도 : O(N제곱) -> 가능  1000 * 1000
+#x+y+z = d -> x+y = d-z
+#3중 for문 안쓰고 2중 for문으로 가능해진다
+
+def function():
+#리스트에 넣는다
+    for i in range(N):
+        temp_num = int(input())
+        element_list.append(temp_num)
+
+
+
+    #정렬한다 -> 이유 ? 나중에 가장 큰 d부터 찾을라고
+    element_list.sort()
+
+    for i in range(N):
+        for j in range(N):
+            sum_set.add(element_list[i] + element_list[j])
+
+    #이제 set 에는 {5,7,12,20, 8 ...} 이런 값이 있다
+    for i in range(N-1, 0, -1):
+        for j in range(i-1, -1, -1):
+            if(element_list[i] - element_list[j]) in sum_set:
+                print(element_list[i])
+                return
+
+
+
+
+
+if __name__ == "__main__":
+    N = int(input())
+
+    element_list = list()
+    sum_set = set()
+
+    function()
--- week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
+++ week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
@@ -5,5 +5,35 @@ from collections import deque
 
 
 
+#abcd
+entire = input().split()
 
-linked_list = deque()
+
+left = deque(entire[0])
+right = deque()
+
+#3
+num = int(input())
+
+for i in range(num):
+    data = input().split()
+
+    if(data[0] == 'L'):
+        if(left):
+            right.appendleft(left.pop())
+    if(data[0] == 'D'):
+        if(right):
+            left.append(right.popleft())
+    if(data[0] == 'B'):
+        if(left):
+            left.pop()
+    if(data[0] == 'P'):
+        left.append(data[1])
+
+# answer = ''
+# for part in left + right:
+#     answer += part
+
+
+# 사용 이유: 한번에 최종 문자열을 생성하여 시간이 빠름
+print(''.join(left+right))
